b32e809ec8ffd916673cc8227a0861c1f14cbc39
Zahid <zahidmohammad495@gmai.com>
2024-01-19 11:01:48 +0530

    rename js/jsx files to ts/tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import  {useEffect} from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {RecoilRoot} from 'recoil';
+import Login from './Components/Login.jsx';
+import Signup from './Components/Signup.jsx';
+import TodoList from './Components/TodoList.jsx';
+import { useNavigate } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
+import { authState } from './store/authState.js';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <RecoilRoot>
+            <Router>
+                <InitState />
+                <Routes>
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/signup' element={<Signup />} />
+                    <Route path='/todos' element={<TodoList />} />
+                    <Route path='/' element={<Login />} />
+                </Routes>
+            </Router>
+        </RecoilRoot>
+    );
 }
 
-export default App
+function InitState() {
+    const setAuth = useSetRecoilState(authState);
+    const navigate = useNavigate();
+
+    const init = async () => {
+        const token = localStorage.getItem("token");
+        try {
+            const response = await fetch('http://localhost:3000/auth/me', {
+                headers: { Authorization: `Bearer ${token}` }
+            });
+            const data = await response.json();
+            if (data.username) {
+                setAuth({ token: data.token, username: data.username });
+                navigate("/todos");
+            } else {
+                navigate("/login");
+            }
+        } catch (e) {
+            navigate("/login");
+        }
+    }
+    useEffect(() => {
+        init();
+    }, [])
+    return <></>
+}
+
+export default App;
+
